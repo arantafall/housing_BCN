c953c7bd56632fc30d77f8bb3ca9d4c17a0fc0f3
Arantafall <aran.tafall.campusimpelia@gmail.com>
2024-12-09 18:48:59 +0100

    borrado el documento python.py que no sirve

--- python.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-data = pd.read_csv("data/Barcelona_Fotocasa_HousingPrices.csv")
-data.rename(columns={'real_state': 'property_type'}, inplace=True)
-data = data.dropna(subset=['property_type'])
-data.reset_index(drop=True, inplace=True)
