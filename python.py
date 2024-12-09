8a58bab65b2e5540d5219c3d5d5eb0d70c4e6dd9
Arantafall <aran.tafall.campusimpelia@gmail.com>
2024-12-09 18:47:04 +0100

    puntos 1 y 3 del documento de analisis

--- python.py
+++ python.py
@@ -4,5 +4,3 @@ data = pd.read_csv("data/Barcelona_Fotocasa_HousingPrices.csv")
 data.rename(columns={'real_state': 'property_type'}, inplace=True)
 data = data.dropna(subset=['property_type'])
 data.reset_index(drop=True, inplace=True)
-
-
